b1baf5efc74e1c0b19b0c9e6ee0f308849c878f8
wazabanda <1wazabanda@gmail.com>
2024-08-26 12:14:41 +0200

    settings

--- IOTCloudServer/settings.py
+++ IOTCloudServer/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_ALLOWED_ORRIGINS = ['https://iotcloudserver-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://iotcloudserver-production.up.railway.app']
 
 # Application definition
 
